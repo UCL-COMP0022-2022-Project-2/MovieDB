76266839457e05e5d6cb8b8f35746ddb0a0657d4
akalivm <2722968218@qq.com>
2022-02-07 18:41:24 +0000

    remove redundant Movie class property 'rating'

--- src/main/java/ucl/comp0022/team2/dao/interfaces/PolarisingDao.java
+++ src/main/java/ucl/comp0022/team2/dao/interfaces/PolarisingDao.java
@@ -4,9 +4,5 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface PolarisingDao {
-    /**
-     * 平均分
-     * @return
-     */
     List<HashMap<Integer, Double>> getPolarisingMovieList();
 }
--- src/main/java/ucl/comp0022/team2/dao/interfaces/PopularDao.java
+++ src/main/java/ucl/comp0022/team2/dao/interfaces/PopularDao.java
@@ -4,19 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface PopularDao {
-    /**
-     * avg
-     * @return
-     */
     List<HashMap<Integer, Double>> getPopularMovieAvgList();
-    /**
-     * Tags数量
-     * @return
-     */
     List<HashMap<Integer, Double>> getPopularMovieTagsList();
-    /**
-     * Ratings数量
-     * @return
-     */
     List<HashMap<Integer, Double>> getPopularMovieRatingsList();
 }
