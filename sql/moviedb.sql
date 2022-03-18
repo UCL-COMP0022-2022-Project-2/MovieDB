32c0e8affab665520e357a82b75e57198624d8bd
gts <gengtangsheng@outlook.com>
2022-03-18 19:06:24 +0000

    update(sql):update sql

--- sql/moviedb.sql
+++ sql/moviedb.sql
@@ -1,100 +1,121 @@
 DROP DATABASE IF EXISTS moviedb;
 CREATE DATABASE IF NOT EXISTS moviedb;
 USE moviedb;
-CREATE TABLE movies
+create table genre_personality
 (
-	movieId INT NOT NULL,
-	title VARCHAR(1000) NOT NULL,
-	genres VARCHAR(1000) NULL,
-	year SMALLINT NOT NULL,
-	CONSTRAINT movies_pk PRIMARY KEY (movieId)
+    genre               char(200)   not null
+        primary key,
+    openness            float(2, 1) not null,
+    agreeableness       float(2, 1) not null,
+    emotional_stability float(2, 1) not null,
+    conscientiousness   float(2, 1) not null,
+    extraversion        float(2, 1) not null
 );
 
-CREATE TABLE links
+create table movies
 (
-	movieId INT NOT NULL,
-	imdbId INT NOT NULL,
-	tmdbId INT NOT NULL,
-	CONSTRAINT links_pk PRIMARY KEY (movieId),
-	CONSTRAINT links_movies_movieId_fk FOREIGN KEY (movieId) REFERENCES movies (movieId)
+    movieId int           not null
+        primary key,
+    title   varchar(1000) not null,
+    genres  varchar(1000) null,
+    year    smallint      not null
 );
 
-CREATE UNIQUE INDEX links_imdbId_uindex ON links (imdbId);
-
-CREATE TABLE ratings
+create table links
 (
-	userId INT NOT NULL,
-	movieId INT NOT NULL,
-	rating FLOAT(2,1) NOT NULL,
-	timestamp TIMESTAMP NOT NULL,
-	CONSTRAINT ratings_movies_movieId_fk FOREIGN KEY (movieId) REFERENCES movies (movieId)
-);
-
-CREATE TABLE tags
-(
-	userId INT NOT NULL,
-	movieId INT NOT NULL,
-	tag TEXT NOT NULL,
-	timestamp TIMESTAMP NOT NULL,
-	CONSTRAINT tags_movies_movieId_fk FOREIGN KEY (movieId) REFERENCES movies (movieId)
+    movieId int not null
+        primary key,
+    imdbId  int not null,
+    tmdbId  int not null,
+    constraint links_imdbId_uindex
+        unique (imdbId),
+    constraint links_movies_movieId_fk
+        foreign key (movieId) references movies (movieId)
 );
 
 create table personality
 (
-    userId              VARCHAR(200) not null,
-    openness            FLOAT(2, 1)   not null,
-    agreeableness       FLOAT(2, 1)   not null,
-    emotional_stability FLOAT(2, 1)   not null,
-    conscientiousness   FLOAT(2, 1)   not null,
-    extraversion        FLOAT(2, 1)   not null,
-    assigned_metric     VARCHAR(100)  not null,
-    assigned_condition  VARCHAR(100)  not null,
-    movie_1             int           not null,
-    predicted_rating_1  float         not null,
-    movie_2             int           not null,
-    predicted_rating_2  float         not null,
-    movie_3             int           not null,
-    predicted_rating_3  float         not null,
-    movie_4             int           not null,
-    predicted_rating_4  FLOAT         not null,
-    movie_5             int           not null,
-    predicted_rating_5  FLOAT         not null,
-    movie_6             int           not null,
-    predicted_rating_6  FLOAT         not null,
-    movie_7             int           not null,
-    predicted_rating_7  FLOAT         not null,
-    movie_8             int           not null,
-    predicted_rating_8  FLOAT         not null,
-    movie_9             int           not null,
-    predicted_rating_9  FLOAT         not null,
-    movie_10            int           not null,
-    predicted_rating_10 FLOAT         not null,
-    movie_11            int           not null,
-    predicted_rating_11 FLOAT         not null,
-    movie_12            int           not null,
-    predicted_rating_12 FLOAT         not null,
-    is_personalized     int           not null,
-    enjoy_watching      int           not null,
-    constraint personality_pk primary key (userId)
+    userId              varchar(200) not null
+        primary key,
+    openness            float(2, 1)  not null,
+    agreeableness       float(2, 1)  not null,
+    emotional_stability float(2, 1)  not null,
+    conscientiousness   float(2, 1)  not null,
+    extraversion        float(2, 1)  not null,
+    assigned_metric     varchar(100) not null,
+    assigned_condition  varchar(100) not null,
+    movie_1             int          not null,
+    predicted_rating_1  float        not null,
+    movie_2             int          not null,
+    predicted_rating_2  float        not null,
+    movie_3             int          not null,
+    predicted_rating_3  float        not null,
+    movie_4             int          not null,
+    predicted_rating_4  float        not null,
+    movie_5             int          not null,
+    predicted_rating_5  float        not null,
+    movie_6             int          not null,
+    predicted_rating_6  float        not null,
+    movie_7             int          not null,
+    predicted_rating_7  float        not null,
+    movie_8             int          not null,
+    predicted_rating_8  float        not null,
+    movie_9             int          not null,
+    predicted_rating_9  float        not null,
+    movie_10            int          not null,
+    predicted_rating_10 float        not null,
+    movie_11            int          not null,
+    predicted_rating_11 float        not null,
+    movie_12            int          not null,
+    predicted_rating_12 float        not null,
+    is_personalized     int          not null,
+    enjoy_watching      int          not null
 );
 
 create table personality_rating
 (
-    userId   char(200) NOT NULL,
-    movie_id int NOT NULL,
-    rating FLOAT(2, 1) NOT NULL,
-    datetime TIMESTAMP,
-    CONSTRAINT personality_rating_userId_fk FOREIGN KEY (userId) REFERENCES personality (userId)
+    userId   char(200)   not null,
+    movie_id int         not null,
+    rating   float(2, 1) not null,
+    datetime timestamp   null,
+    constraint personality_rating_userId_fk
+        foreign key (userId) references personality (userId)
 );
 
-create table genre_personality
+create table predicted_rating
 (
-    genre               char(200) NOT NULL,
-    openness            FLOAT(2, 1)   not null,
-    agreeableness       FLOAT(2, 1)   not null,
-    emotional_stability FLOAT(2, 1)   not null,
-    conscientiousness   FLOAT(2, 1)   not null,
-    extraversion        FLOAT(2, 1)   not null,
-    CONSTRAINT genre_personality_fk primary key  (genre)
+    movieId int unsigned not null
+        primary key,
+    rating  double(3, 2) null
+);
+
+create table ratings
+(
+    userId    int         not null,
+    movieId   int         not null,
+    rating    float(2, 1) not null,
+    timestamp timestamp   not null,
+    constraint ratings_movies_movieId_fk
+        foreign key (movieId) references movies (movieId)
+);
+
+create table tag_personality
+(
+    tag                 varchar(100) null,
+    openness            double(3, 2) null,
+    agreeableness       double(3, 2) null,
+    emotional_stability double(3, 2) null,
+    conscientiousness   double(3, 2) null,
+    extraversion        double(3, 2) null
+);
+
+create table tags
+(
+    userId    int       not null,
+    movieId   int       not null,
+    tag       text      not null,
+    timestamp timestamp not null,
+    constraint tags_movies_movieId_fk
+        foreign key (movieId) references movies (movieId)
 );
 
