10950cd008dddd2ab13122872275354244e7ae82
gts <gengtangsheng@outlook.com>
2022-03-20 16:32:50 +0000

    feat(frontend):basically accomplish task 5 and 6

--- src/main/webapp/static/js/predict.js
+++ src/main/webapp/static/js/predict.js
@@ -2,6 +2,9 @@ window.onload = initial;
 var contextPath = $("#contextPath").val();
 var chart = $("#charts");
 var report = $("#reports");
+var resultsTextsArray = [];
+var totalAverageScoreArray = [];
+var resultsAverageScoreArray = [];
 function initial() {
     $("#genreContent").hide()
     $("#content").show()
@@ -99,12 +102,9 @@ $("#tagSubmit").on("click", function () {
         tagString[i] = $(".tagInbox").eq(i).text();
     }
     clearAll();
-
-    var resultsTextsArray = [];
-    var totalAverageScoreArray = [];
-    var resultsAverageScoreArray;
     $.ajax({
         url: contextPath + "/getTotalTagsAverageRatings.do",
+        async: false,
         success(resp) {
             totalAverageScoreArray[0] = resp.openness;
             totalAverageScoreArray[1] = resp.agreeableness;
@@ -116,21 +116,25 @@ $("#tagSubmit").on("click", function () {
     $.ajax({
         url: contextPath + "/getRatingsByTags.do",
         traditional: true,
+        async: false,
         data: {
             tags: tagString
         },
         success(resp) {
             var theTagsAverage = resp.theTotalAverage;
-            resultsAverageScoreArray = [theTagsAverage.openness.data, theTagsAverage.agreeableness.data, theTagsAverage.emotional_stability.data, theTagsAverage.conscientiousness.data, theTagsAverage.extraversion.data];
+            resultsAverageScoreArray[0] = theTagsAverage.openness;
+            resultsAverageScoreArray[1] = theTagsAverage.agreeableness;
+            resultsAverageScoreArray[2] = theTagsAverage.emotional_stability;
+            resultsAverageScoreArray[3] = theTagsAverage.conscientiousness;
+            resultsAverageScoreArray[4] = theTagsAverage.extraversion;
             for(var i in resp){
                 if(i !== "theTotalAverage"){
-                    resultsTextsArray.push(i.toString());
+                    resultsTextsArray.push(i);
                 }
             }
         }
     });
-    console.log(resultsAverageScoreArray);
-    displayResult('tags', resultsTextsArray, totalAverageScoreArray, resultsAverageScoreArray)
+    displayResult('tags')
 });
 
 $("#genreSubmit").on("click", function () {
@@ -141,23 +145,41 @@ $("#genreSubmit").on("click", function () {
     clearAll();
     $.ajax({
         url: contextPath + "/getTotalGenresAverageRatings.do",
+        async: false,
         success(resp) {
-            console.log(resp)
+            totalAverageScoreArray[0] = resp.openness;
+            totalAverageScoreArray[1] = resp.agreeableness;
+            totalAverageScoreArray[2] = resp.emotional_stability;
+            totalAverageScoreArray[3] = resp.conscientiousness;
+            totalAverageScoreArray[4] = resp.extraversion;
         }
     });
     $.ajax({
         url: contextPath + "/getRatingsByGenres.do",
         traditional: true,
+        async: false,
         data: {
             genres: genreString
         },
         success(resp) {
-            console.log(resp)
+            var theGenresAverage = resp.theTotalAverage;
+            resultsAverageScoreArray[0] = theGenresAverage.openness;
+            resultsAverageScoreArray[1] = theGenresAverage.agreeableness;
+            resultsAverageScoreArray[2] = theGenresAverage.emotional_stability;
+            resultsAverageScoreArray[3] = theGenresAverage.conscientiousness;
+            resultsAverageScoreArray[4] = theGenresAverage.extraversion;
+            for(var i in resp){
+                if(i !== "theTotalAverage"){
+                    resultsTextsArray.push(i);
+                }
+            }
         }
-    })
+    });
+    displayResult('genres')
 });
 $("#clear").on("click", function () {
-    clearAll()
+    $(".tagInBox").remove();
+    $(".genreInBox").remove();
 });
 
 function clearAll() {
@@ -165,12 +187,15 @@ function clearAll() {
     $(".genreInBox").remove();
     chart.empty();
     report.empty();
+    resultsTextsArray = [];
+    totalAverageScoreArray = [];
+    resultsAverageScoreArray = [];
 }
-function displayResult(tagsOrGenres, resultsTextsArray, totalAverageScoreArray, resultsAverageScoreArray){
-    displayChart(tagsOrGenres, totalAverageScoreArray, resultsAverageScoreArray);
-    displayReport(tagsOrGenres, resultsTextsArray, totalAverageScoreArray, resultsAverageScoreArray);
+function displayResult(tagsOrGenres){
+    displayChart(tagsOrGenres);
+    displayReport(tagsOrGenres);
 }
-function displayChart(tagsOrGenres, totalAverageScoreArray, resultsAverageScoreArray) {
+function displayChart(tagsOrGenres) {
     var mainChart = $("<canvas id=\"mainChart\"></canvas>");
     chart.append(mainChart);
     const labels = [
@@ -180,7 +205,7 @@ function displayChart(tagsOrGenres, totalAverageScoreArray, resultsAverageScoreA
         'Conscientiousness',
         'Extraversion'
     ];
-    const data = {
+    var data = {
         labels: labels,
         datasets: [{
             label: 'Average score for all ' + tagsOrGenres,
@@ -204,6 +229,29 @@ function displayChart(tagsOrGenres, totalAverageScoreArray, resultsAverageScoreA
         config
     );
 }
-function displayReport(tagsOrGenres, resultsTextsArray, totalAverageScoreArray, resultsAverageScoreArray){
-
+function displayReport(tagsOrGenres){
+    var line1 = $("<h1 class = \"display-6\" ></h1>").text("The " + tagsOrGenres +" selected by you are:");
+    var line2 = $("<h1 class = \"display-6\" ></h1>").text(resultsTextsArray.join(", "));
+    var line3 = $("<h1 class = \"display-6\" ></h1>").text("The average personality score of all " + tagsOrGenres + "is:");
+    var line4 = $("<h1 class = \"display-6\" ></h1>").text("openness: " + totalAverageScoreArray[0] + "," +
+        "agreeableness " + totalAverageScoreArray[1] + "," +
+        "emotional stability: " + totalAverageScoreArray[2] + "," +
+        "conscientiousness: " + totalAverageScoreArray[3] + "," +
+        "extraversion: " + totalAverageScoreArray[4]);
+    var line5 = $("<h1 class = \"display-6\" ></h1>").text("The average personality score of selected " + tagsOrGenres + "is:");
+    var line6 = $("<h1 class = \"display-6\" ></h1>").text("openness: " + resultsAverageScoreArray[0] + "," +
+        "agreeableness " + resultsAverageScoreArray[1] + "," +
+        "emotional stability: " + resultsAverageScoreArray[2] + "," +
+        "conscientiousness: " + resultsAverageScoreArray[3] + "," +
+        "extraversion: " + resultsAverageScoreArray[4]);
+    var line7 = $("<h1 class = \"display-6\" ></h1>").text("According to the statistics above, it is likely that the movie with selected" + tagsOrGenres + "will be favored by people who are");
+    var line8 = $("<h1 class = \"display-6\" ></h1>").text();
+    report.append(line1);
+    report.append(line2);
+    report.append(line3);
+    report.append(line4);
+    report.append(line5);
+    report.append(line6);
+    report.append(line7);
+    report.append(line8);
 }
